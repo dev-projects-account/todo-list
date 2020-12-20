e444daf81405b85f7d0c0d7e82c34e5579c29225
Aquarius-beans <onetimeuser38@gmail.com>
2020-12-20 11:30:37 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      enteredGoalValue: "",
+      goals: [],
+    };
+  },
+  methods: {
+    addGoal() {
+      this.goals.push(this.enteredGoalValue);
+      console.log(this.goals);
+    },
+    removeGoal(index) {
+      this.goals.splice(index, 1);
+    },
+  },
+});
+
+app.mount("#user-goals");
